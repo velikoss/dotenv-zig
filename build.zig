49fb9fc3e6a9806c3b301f2fadebd46f704357ec
Velikiy Kirill <kuklix@ya.ru>
2024-06-12 17:27:31 +0300

    Update build.zig

--- build.zig
+++ build.zig
@@ -20,7 +20,7 @@ pub fn build(b: *std.Build) void {
 	});
 
     const lib = b.addStaticLibrary(.{
-        .name = "dotenv-zig",
+        .name = "dotenv",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/root.zig"),
